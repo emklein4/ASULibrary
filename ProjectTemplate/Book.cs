8b114640c62225cfb1ff4727b038a75d673a4913
Ethan <emklein4@asu.edu>
2020-02-17 10:37:28 -0700

    new book class and load books

---
+++ ProjectTemplate/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectTemplate
+{
+    public class Book
+    {
+        public string isbn;
+        public string title;
+        public string author;
+        public int quantity;
+    }
+}
